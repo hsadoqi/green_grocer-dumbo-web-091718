e231bb70527116d7db7689a527bcfde828dfbcaf
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-28 22:00:48 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -39,7 +39,7 @@ def apply_clearance(cart)
   cart.each do |item, info|
     info.each do |key|
     if cart[item][:clearance] == true
-      cart[item][:price] * (0.80).round(2)
+      (cart[item][:price] * 0.80).round(2)
     else 
       clearance_cart[item][:price] = cart[item][:price]
     end 
