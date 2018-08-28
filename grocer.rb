545fb7a669176e98ac4e5c1ec9a88ecdf318cda2
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-28 21:42:30 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,23 @@
 def consolidate_cart(cart)
   # code here
+  cart_hash = {}
+  cart.each do |item|
+    item.each do |name, value|
+      if cart_hash[name]
+        cart_hash[name][:count] += 1 
+      else 
+        cart_hash[name] = value
+        cart_hash[name][:count] = 1 
+      end 
+    end 
+  end
+  cart_hash
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  if coupons[:item] == cart[name]
+    cart[name][:price] = coupons[:]
 end
 
 def apply_clearance(cart)
